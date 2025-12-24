328bdfb30eb96cddf87ebf06b567c7a28394f615
EsraaNasr92 <esraanasr706@gmail.com>
2025-12-24 12:34:16 +0200

    feat: Add tabs for the project section.

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,24 @@ burger.addEventListener('click', () => {
     menu.classList.toggle('active');
     burger.classList.toggle('active');
 
-    // Update aria-expanded for accessibility
     const expanded = burger.getAttribute('aria-expanded') === 'true';
     burger.setAttribute('aria-expanded', !expanded);
+});
+
+const filterButtons = document.querySelectorAll('.project-filters button');
+const projectCards = document.querySelectorAll('.project-cards');
+
+filterButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Active button
+        filterButtons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+
+        const filter = button.dataset.filter;
+
+        projectCards.forEach(card => {
+            const type = card.dataset.type;
+            card.style.display = (filter === 'all' || filter === type) ? 'flex' : 'none';
+        });
+    });
 });
